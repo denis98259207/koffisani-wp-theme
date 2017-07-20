78445b7328e19bc74dac974da0f275ba3b725041
Koffi Sani <koffisani@gmail.com>
2017-07-20 20:24:22 +0100

    Debug unit test and CI on another branch

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,25 +1,34 @@
 <?php
+/**
+ * PHPUnit bootstrap file
+ *
+ * @package Koffisani_Wp_Theme
+ */
 
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
 if ( ! $_tests_dir ) {
 	$_tests_dir = '/tmp/wordpress-tests-lib';
 }
 
-define('PLUGIN_NAME','koffisani');
-define('PLUGIN_FOLDER',basename(dirname( __DIR__ )));
-define('PLUGIN_PATH',PLUGIN_FOLDER.'/'.PLUGIN_NAME);
-
-// Activates this plugin in WordPress so it can be tested.
-$GLOBALS['wp_tests_options'] = array(
-  'active_plugins' => array( PLUGIN_PATH ),
-);
-
+// Give access to tests_add_filter() function.
 require_once $_tests_dir . '/includes/functions.php';
 
-function _manually_load_plugin() {
-	require dirname( __DIR__ ) . '/'.PLUGIN_NAME;
+function _register_theme() {
+
+	$theme_dir = dirname( dirname( __FILE__ ) );
+	$current_theme = basename( $theme_dir );
+
+	register_theme_directory( dirname( $theme_dir ) );
+
+	add_filter( 'pre_option_template', function() use ( $current_theme ) {
+		return $current_theme;
+	});
+	add_filter( 'pre_option_stylesheet', function() use ( $current_theme ) {
+		return $current_theme;
+	});
 }
+tests_add_filter( 'muplugins_loaded', '_register_theme' );
 
-tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
+// Start up the WP testing environment.
 require $_tests_dir . '/includes/bootstrap.php';
--- tests/test-sample.php
+++ tests/test-sample.php
@@ -1,9 +1,20 @@
 <?php
+/**
+ * Class SampleTest
+ *
+ * @package Koffisani_Wp_Theme
+ */
+
+/**
+ * Sample test case.
+ */
 class SampleTest extends WP_UnitTestCase {
-	function testSample() {
-            $theme = wp_get_theme();
-		//$this->assertTrue( 'koffisani' == $theme->get( 'Name' ) );
-		//$this->assertTrue( is_plugin_active('your-plugin/your-plugin.php') );
-                $this->assertTrue( TRUE );
+
+	/**
+	 * A single example test.
+	 */
+	function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
 	}
 }
