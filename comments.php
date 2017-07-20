4f27a40583e04ac2069880501962f445f7a6e5c6
Koffi Sani <koffisani@gmail.com>
2017-07-21 00:56:42 +0100

    Fix errors of CI

--- comments.php
+++ comments.php
@@ -22,63 +22,63 @@ if ( post_password_required() ) {
 
 <div id="comments" class="comments-area col-xs-12 col-sm-12 col-md-12 col-lg-12">
 
-	<?php
-	// You can start editing here -- including this comment!
-	if ( have_comments() ) : ?>
-		<h2 class="comments-title">
-			<?php
-				printf( // WPCS: XSS OK.
-                                        'Commentaires (' . number_format_i18n(get_comments_number()) . ')'
-				);
-			?>
-		</h2><!-- .comments-title -->
+    <?php
+    // You can start editing here -- including this comment!
+    if ( have_comments() ) : ?>
+        <h2 class="comments-title">
+            <?php
+                printf( // WPCS: XSS OK.
+                    'Commentaires (' . number_format_i18n (get_comments_number ()) . ')'
+                );
+            ?>
+        </h2><!-- .comments-title -->
 
-		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
-		<nav id="comment-nav-above" class="navigation comment-navigation" role="navigation">
-			<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'koffisani' ); ?></h2>
-			<div class="nav-links">
+        <?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
+        <nav id="comment-nav-above" class="navigation comment-navigation" role="navigation">
+                <h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'koffisani' ); ?></h2>
+                <div class="nav-links">
 
-				<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'koffisani' ) ); ?></div>
-				<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', 'koffisani' ) ); ?></div>
+                        <div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'koffisani' ) ); ?></div>
+                        <div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', 'koffisani' ) ); ?></div>
 
-			</div><!-- .nav-links -->
-		</nav><!-- #comment-nav-above -->
-		<?php endif; // Check for comment navigation. ?>
+                </div><!-- .nav-links -->
+        </nav><!-- #comment-nav-above -->
+        <?php endif; // Check for comment navigation. ?>
 
-		<ol id="comment-list">
-			<?php
-				wp_list_comments( array(
-					'style'      => 'ul',
-					'short_ping' => true,
-                                        'avatar_size' => 74
-				) );
-			?>
-		</ol><!-- .comment-list -->
+        <ol id="comment-list">
+                <?php
+                    wp_list_comments( array(
+                        'style'      => 'ul',
+                        'short_ping' => true,
+                        'avatar_size' => 74,
+                    ) );
+                ?>
+        </ol><!-- .comment-list -->
 
-		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
-		<nav id="comment-nav-below" class="navigation comment-navigation" role="navigation">
-			<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'koffisani' ); ?></h2>
-			<div class="nav-links">
+        <?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
+        <nav id="comment-nav-below" class="navigation comment-navigation" role="navigation">
+                <h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'koffisani' ); ?></h2>
+                <div class="nav-links">
 
-				<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'koffisani' ) ); ?></div>
-				<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', 'koffisani' ) ); ?></div>
+                        <div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'koffisani' ) ); ?></div>
+                        <div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', 'koffisani' ) ); ?></div>
 
-			</div><!-- .nav-links -->
-		</nav><!-- #comment-nav-below -->
-		<?php
-		endif; // Check for comment navigation.
+                </div><!-- .nav-links -->
+        </nav><!-- #comment-nav-below -->
+        <?php
+        endif; // Check for comment navigation.
 
-	endif; // Check for have_comments().
+    endif; // Check for have_comments().
 
 
-	// If comments are closed and there are comments, let's leave a little note, shall we?
-	if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) : ?>
+    // If comments are closed and there are comments, let's leave a little note, shall we?
+    if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) : ?>
 
-		<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'koffisani' ); ?></p>
-	<?php
-	endif;
+            <p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'koffisani' ); ?></p>
+    <?php
+    endif;
 
-	koffisani_comment_form();
-	?>
+    koffisani_comment_form();
+    ?>
 
 </div><!-- #comments -->
